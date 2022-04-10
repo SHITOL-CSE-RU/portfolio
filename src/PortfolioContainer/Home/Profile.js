243e92838d775f82653fb305023b36a25d84d9ef
SHITOL-CSE-RU <shitol.cse@gmail.com>
2022-04-10 23:25:16 +0600

    First Section Done

--- src/PortfolioContainer/Home/Profile.js
+++ src/PortfolioContainer/Home/Profile.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Typical from "react-typical";
+import "./Profile.css";
 
 export default function Profile() {
   return (
@@ -7,21 +8,23 @@ export default function Profile() {
       <div className="profile-parent">
         <div className="profile-details">
           <div className="colz">
-            <a href="https://www.facebook.com/shitol.cse">
-              <i className="fa fa-facebook-square"></i>
-            </a>
-            <a href="https://twitter.com/shitolcse">
-              <i className="fa fa-twitter-square"></i>
-            </a>
-            <a href="https://www.instagram.com/shitol.cse">
-              <i className="fa fa-instagram"></i>
-            </a>
-            <a href="https://www.linkedin.com/in/shitol-sarker-9b3824218/">
-              <i class="fa fa-linkedin-square"></i>
-            </a>
-            <a href="https://github.com/SHITOL-CSE-RU">
-              <i class="fa fa-github-square"></i>
-            </a>
+            <div className="colz-icon">
+              <a href="https://www.facebook.com/shitol.cse">
+                <i className="fa fa-facebook-square"></i>
+              </a>
+              <a href="https://twitter.com/shitolcse">
+                <i className="fa fa-twitter-square"></i>
+              </a>
+              <a href="https://www.instagram.com/shitol.cse">
+                <i className="fa fa-instagram"></i>
+              </a>
+              <a href="https://www.linkedin.com/in/shitol-sarker-9b3824218/">
+                <i class="fa fa-linkedin-square"></i>
+              </a>
+              <a href="https://github.com/SHITOL-CSE-RU">
+                <i class="fa fa-github-square"></i>
+              </a>
+            </div>
           </div>
           <div className="profile-details-name">
             <span className="primary-text">
@@ -48,8 +51,23 @@ export default function Profile() {
                   ]}
                 />
               </h1>
+              <span class="profile-role-tagline">
+                Knack of building applications with front and back end
+                operations.
+              </span>
             </span>
           </div>
+          <div className="profile-options">
+            <button className="btn primary-btn"> Hire Me </button>
+            <a href="MyResume.pdf" download="Shitol MyResume.pdf">
+              <button className="btn highlighted-btn">Get Resume</button>
+            </a>
+          </div>
+        </div>
+        <div className="profile-picture">
+            <div className="profile-picture-background">
+
+            </div>
         </div>
       </div>
     </div>
