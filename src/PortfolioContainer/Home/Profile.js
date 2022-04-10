bf6c04235955657b016b3a8b554f593f77a5b57f
SHITOL-CSE-RU <shitol.cse@gmail.com>
2022-04-10 15:19:04 +0600

    fontawesome icon add

---
+++ src/PortfolioContainer/Home/Profile.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+export default function Profile(){
+    return (
+        <div className="profile-container">
+            <div className="profile-parent">
+                <div className="profile-details">
+                    <div className="colz">
+                        <a href="#">
+                            <i className="fa fa-facebook-square"></i>
+                        </a>
+                        <a href="#">
+                            <i className="fa fa-twitter-square"></i>
+                        </a>
+                        <a href="#">
+                            <i className="fa fa-linkedin-square"></i>
+                        </a>
+                        <a href="#">
+                            <i class="fa fa-youtube-square"></i>
+                        </a>
+                        <a href="#">
+                            <i class="fa fa-github-square"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+
+}
